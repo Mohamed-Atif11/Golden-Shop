e4476e7d99f44a174f87e81be622244a2a097bde
Mohamed-Atif11 <mhmmd.attff@gmail.com>
2023-08-10 00:35:39 +0200

    Atif

---
+++ main.js
@@ -0,0 +1,16 @@
+var $mood = document.getElementById("mode");
+function changemode(){
+    $mood.style.backgroundColor="#00072d";
+    $mood.style.color="black";
+    $mood.style.fontWeight="bold";
+
+  }
+  var $light =document.getElementsById("mode");
+  function changetolight(){
+    $mood.style.backgroundColor="#e9ecef";
+    $mood.style.color="black";
+    $mood.style.fontWeight="normal";
+  }
+
+  // 
+  
